7b76ad19f23ba789073dd05765f84ee147de6127
slpencer <susan.spencer@gmail.com>
2022-08-26 12:44:59 -0500

    add extra . to db.json path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,13 +6,13 @@ const router = require("express").Router();
 
 // define router for reading all notes
 router.get("/notes", (req, res) => {
-    const db = fs.readFileSync("./db/db.json"); // get current notes, jsonified
+    const db = fs.readFileSync("../db/db.json"); // get current notes, jsonified
     res.json(JSON.parse(db));  // return the jsonified notes
 });
 
 // define router for deleting a note
 router.delete("notes/:id", (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync("./db/db.json")); // get current notes, jsonified
+    const dbJSON = JSON.parse(fs.readFileSync("../db/db.json")); // get current notes, jsonified
     const keepNotes = dbJSON.filter((keepNote) => keepNote.id !== req.params.id); // filter out the node with matching id
     fs.writeFileSync("./db/db.json", JSON.stringify(keepNotes)); // save the filtered notes back to the database
     res.json(keepNotes); // return jsonified notes, without the deleted note
@@ -20,11 +20,11 @@ router.delete("notes/:id", (req, res) => {
 
 // define router for adding a note
 router.post("/notes", (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync("./db/db.json"));
+    const dbJSON = JSON.parse(fs.readFileSync("../db/db.json"));
     const addNote = req.body; // create an object from the parameter
     addNote.id = uuid.v4(); // assign a unique id
     dbJSON.push(addNote); // push the object to the jsonified notes
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes)); // save the jsonified notes to the database
+    fs.writeFileSync("../db/db.json", JSON.stringify(notes)); // save the jsonified notes to the database
     res.json(dbJSON); // return jsonified notes, with the new note
 });
 
